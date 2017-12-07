9008df485c9f41d92cc23aa722913de7bb75da16
Ken Chen <kchen@splunk.com>
2017-12-07 15:37:28 -0800

    fix license comments

--- src/main/java/com/splunk/hecclient/DoubleSerializer.java
+++ src/main/java/com/splunk/hecclient/DoubleSerializer.java
@@ -8,8 +8,20 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import java.io.IOException;
 import java.math.BigDecimal;
 
-/**
- * Created by kchen on 12/7/17.
+/*
+ * Copyright 2017 Splunk, Inc..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 public class DoubleSerializer extends JsonSerializer<Double> {
     @Override
