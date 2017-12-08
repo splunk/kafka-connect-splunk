e2f3d3804ffda1b26dc15313763c5ea20fdbaf64
Ken Chen <kchen@splunk.com>
2017-12-08 10:19:54 -0800

    add license header in UT

--- src/test/java/com/splunk/hecclient/DoubleSerializerTest.java
+++ src/test/java/com/splunk/hecclient/DoubleSerializerTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 Splunk, Inc..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.splunk.hecclient;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -6,9 +21,6 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Created by kchen on 12/7/17.
- */
 public class DoubleSerializerTest {
     @JsonSerialize(using = DoubleSerializer.class)
     private Double d;
