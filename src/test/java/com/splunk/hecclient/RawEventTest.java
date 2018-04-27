9a67c17fc2e441d5ff53636dbf414a4db4802a09
Jeremy Custenborder <jcustenborder@gmail.com>
2018-04-27 09:41:17 -0500

    Added serializer for Struct which is the class structured data will be passed to the connector as. Set the date format for the ObjectMapper to ISO 8601. Added a test case with nested structs. Fixes #134.

--- src/test/java/com/splunk/hecclient/RawEventTest.java
+++ src/test/java/com/splunk/hecclient/RawEventTest.java
@@ -16,11 +16,18 @@
 package com.splunk.hecclient;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.data.Timestamp;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class RawEventTest {
@@ -42,6 +49,35 @@ public class RawEventTest {
 
         Assert.assertEquals(event.getTied(), tied);
         Assert.assertEquals(event.getEvent(), data);
+    }
+
+    @Test
+    public void struct() {
+        final Schema childSchema = SchemaBuilder.struct()
+            .name("child")
+            .field("first_name", Schema.STRING_SCHEMA)
+            .field("age", Schema.INT32_SCHEMA)
+            .build();
+        final Schema parentSchema = SchemaBuilder.struct()
+            .name("test")
+            .field("first_name", Schema.STRING_SCHEMA)
+            .field("count", Schema.INT32_SCHEMA)
+            .field("timestamp", Timestamp.SCHEMA)
+            .field("children", SchemaBuilder.array(childSchema).build())
+            .build();
+        final List<Struct> children = Arrays.asList(
+            new Struct(childSchema).put("first_name", "Thing 1").put("age", 4),
+            new Struct(childSchema).put("first_name","Thing 2").put("age", 7)
+        );
+        final Struct struct = new Struct(parentSchema)
+            .put("first_name", "fred")
+            .put("count", 1234)
+            .put("timestamp", new Date(1524838717123L))
+            .put("children", children);
+        final Event event = new RawEvent(struct, null);
+        final String expected = "{\"first_name\":\"fred\",\"count\":1234,\"timestamp\":\"2018-04-27T14:18:37.123+0000\",\"children\":[{\"first_name\":\"Thing 1\",\"age\":4},{\"first_name\":\"Thing 2\",\"age\":7}]}";
+        final String actual = event.toString();
+        Assert.assertEquals(expected, actual);
     }
 
     @Test(expected = HecException.class)
