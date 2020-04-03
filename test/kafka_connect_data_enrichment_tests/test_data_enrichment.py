1ece914fba85bf858e4945c04d3c4336519a8681
Rock Baek <rbaek@splunk.com>
2020-04-02 22:16:34 -0700

    fix

--- test/kafka_connect_data_enrichment_tests/test_data_enrichment.py
+++ test/kafka_connect_data_enrichment_tests/test_data_enrichment.py
@@ -25,7 +25,7 @@ def test_data_enrichment_latin1(setup, test_scenario, test_input, expected):
     logger.info("testing {0} input={1} expected={2} event(s)".format(
         test_scenario, test_input, expected))
     search_query = "index=" + setup['splunk_index'] + "chars::" + test_input 
-    events = check_events_from_splunk(index=start_time="-1h@h",
+    events = check_events_from_splunk(start_time="-1h@h",
                                       url=setup["splunkd_url"],
                                       user=setup["splunk_user"],
                                       query=["search {0}".format(
