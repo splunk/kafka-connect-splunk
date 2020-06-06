0b0b29fdf90d32cd0841e23cbd4b8d9908e8d8ba
Mei <meiw@splunk.com>
2020-06-05 17:29:09 -0700

    add build kafka and kafka connect into ci pipeline
    add build image for kafka connect integration tests into ci pipeline

--- ci/run_kafka_connect.sh
+++ ci/run_kafka_connect.sh
@@ -1,39 +1,40 @@
 #!/bin/bash
 
+CI_KAFKA_TOPIC=test-datagen
 # Checkout, build and run kafka-connect-splunk in the fight
 
 curdir=`pwd`
-git clone git@github.com:splunk/kafka-connect-splunk.git
+git clone https://github.com/splunk/kafka-connect-splunk.git
 
 branch=${KAFKA_CONNECT_BRANCH:-develop}
 # build the package
 cd kafka-connect-splunk && git checkout ${branch} && bash build.sh
+cd /kafka-connect
+cp kafka-connect-splunk/target/splunk-kafka-connect-v*.jar /kafka-connect/
+yes | cp -rf config.yaml kafka-connect-splunk/test/config.yaml
 
-# untar the package
-tar xzf kafka-connect-splunk*.tar.gz
-cd kafka-connect-splunk
+sed -i 's/plugin.path=connectors/plugin.path=\/kafka-connect/' /kafka-connect/kafka-connect-splunk/config/connect-distributed-quickstart.properties
+sed -i 's/key.converter=org.apache.kafka.connect.storage.StringConverter/key.converter=org.apache.kafka.connect.json.JsonConverter/' /kafka-connect/kafka-connect-splunk/config/connect-distributed-quickstart.properties
+sed -i 's/value.converter=org.apache.kafka.connect.storage.StringConverter/value.converter=org.apache.kafka.connect.json.JsonConverter/' /kafka-connect/kafka-connect-splunk/config/connect-distributed-quickstart.properties
 
-sed -i"" "s@bootstrap.servers=.*@bootstrap.servers=$KAFKA_BOOTSTRAP_SERVERS@g" config/connect-distributed.properties
+#debug=${KAFKA_CONNECT_LOGGING:-DEBUG}
+#echo "log4j.logger.com.splunk=${debug}" >> config/connect-log4j.properties
 
-debug=${KAFKA_CONNECT_LOGGING:-DEBUG}
-echo "log4j.logger.com.splunk=${debug}" >> config/connect-log4j.properties
+cd kafka
 
-git clone https://github.com/chenziliang/proc_monitor && git checkout develop
+echo "Start ZooKeeper"
+bin/zookeeper-server-start.sh config/zookeeper.properties > /kafka-connect/logs/zookeeper.txt 2>&1 &
 
-duration=${SLEEP:-300}
-sleep ${duration}
-
-echo "Run fix hosts"
-bash ${curdir}/kafka-connect-splunk/ci/fix_hosts.sh > /tmp/fixhosts 2>&1 &
-
-echo "Run proc monitor"
-cd proc_monitor
-python proc_monitor.py 2>&1 &
-cd ..
+echo "Start kafka server"
+bin/kafka-server-start.sh config/server.properties > /kafka-connect/logs/kafka.txt 2>&1 &
 
 echo "Run connect"
-while :
-do
-    ./bin/connect-distributed.sh config/connect-distributed.properties
-    sleep 1
-done
+./bin/connect-distributed.sh /kafka-connect/kafka-connect-splunk/config/connect-distributed-quickstart.properties > /kafka-connect/logs/kafka_connect.txt 2>&1 &
+
+echo "-----------------run integration tests-----------------"
+cd /kafka-connect/kafka-connect-splunk/test
+pip install virtualenv
+virtualenv venv
+pip install -r requirements.txt
+source venv/bin/activate
+pytest
