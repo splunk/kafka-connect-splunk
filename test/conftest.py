1fa5a4f169addc52a1f7dae1cd936bf3a95b258b
Mei <meiw@splunk.com>
2020-07-19 16:06:55 -0700

    test

--- test/conftest.py
+++ test/conftest.py
@@ -15,13 +15,12 @@ limitations under the License.
 """
 from lib.commonkafka import *
 from lib.connect_params import *
-from datetime import datetime
+
 from kafka.producer import KafkaProducer
 from lib.helper import get_test_folder
 from lib.data_gen import generate_connector_content
 import pytest
 import yaml
-import uuid
 
 logging.config.fileConfig(os.path.join(get_test_folder(), "logging.conf"))
 logger = logging.getLogger(__name__)
@@ -46,31 +45,28 @@ def pytest_configure():
                              value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
     for _ in range(3):
-        id_1 = str(uuid.uuid1())
-        msg = {"timestamp": config['timestamp'], "_id": id_1}
+        msg = {"timestamp": config['timestamp']}
         producer.send(config["kafka_topic"], msg)
-        id_2 = str(uuid.uuid1())
-        msg_2 = {"timestamp": str(datetime.now()), "_id": id_2}
-        producer.send(config["kafka_topic_2"], msg_2)
-    #
-    #     headers_to_send = [('header_index', b'kafka'), ('header_source_event', b'kafka_header_source_event'),
-    #                        ('header_host_event', b'kafkahostevent.com'),
-    #                        ('header_sourcetype_event', b'kafka_header_sourcetype_event')]
-    #     producer.send(config["kafka_header_topic"], msg, headers=headers_to_send)
-    #
-    #     headers_to_send = [('header_index', b'kafka'), ('header_source_raw', b'kafka_header_source_raw'),
-    #                        ('header_host_raw', b'kafkahostraw.com'),
-    #                        ('header_sourcetype_raw', b'kafka_header_sourcetype_raw')]
-    #     producer.send(config["kafka_header_topic"], msg, headers=headers_to_send)
-    #
-    #     headers_to_send = [('splunk.header.index', b'kafka'),
-    #                        ('splunk.header.host', b'kafkahost.com'),
-    #                        ('splunk.header.source', b'kafka_custom_header_source'),
-    #                        ('splunk.header.sourcetype', b'kafka_custom_header_sourcetype')]
-    #     producer.send(config["kafka_header_topic"], msg, headers=headers_to_send)
-    #
-    # producer.send("test_splunk_hec_malformed_events", {})
-    # producer.send("test_splunk_hec_malformed_events", {"&&": "null", "message": ["$$$$****////", 123, None]})
+        producer.send(config["kafka_topic_2"], msg)
+
+        headers_to_send = [('header_index', b'kafka'), ('header_source_event', b'kafka_header_source_event'),
+                           ('header_host_event', b'kafkahostevent.com'),
+                           ('header_sourcetype_event', b'kafka_header_sourcetype_event')]
+        producer.send(config["kafka_header_topic"], msg, headers=headers_to_send)
+
+        headers_to_send = [('header_index', b'kafka'), ('header_source_raw', b'kafka_header_source_raw'),
+                           ('header_host_raw', b'kafkahostraw.com'),
+                           ('header_sourcetype_raw', b'kafka_header_sourcetype_raw')]
+        producer.send(config["kafka_header_topic"], msg, headers=headers_to_send)
+
+        headers_to_send = [('splunk.header.index', b'kafka'),
+                           ('splunk.header.host', b'kafkahost.com'),
+                           ('splunk.header.source', b'kafka_custom_header_source'),
+                           ('splunk.header.sourcetype', b'kafka_custom_header_sourcetype')]
+        producer.send(config["kafka_header_topic"], msg, headers=headers_to_send)
+
+    producer.send("test_splunk_hec_malformed_events", {})
+    producer.send("test_splunk_hec_malformed_events", {"&&": "null", "message": ["$$$$****////", 123, None]})
     producer.flush()
 
     # Launch all connectors for tests
@@ -79,7 +75,7 @@ def pytest_configure():
         create_kafka_connector(config, connector_content)
 
     # wait for data to be ingested to Splunk
-    time.sleep(200)
+    time.sleep(60)
 
 
 def pytest_unconfigure():
