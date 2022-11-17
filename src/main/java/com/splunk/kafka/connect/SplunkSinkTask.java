38486af9351447508a2d92e87e8f17e67a3a7bd8
naveenmall11 <nmall@confluent.io>
2022-11-17 15:37:46 +0530

    remove logging of record value

--- src/main/java/com/splunk/kafka/connect/SplunkSinkTask.java
+++ src/main/java/com/splunk/kafka/connect/SplunkSinkTask.java
@@ -401,7 +401,7 @@ public final class SplunkSinkTask extends SinkTask implements PollerCallback {
                 event.setTied(record);
                 event.addFields(connectorConfig.enrichments);
             } catch(Exception e) {
-                log.trace("event does not follow correct HEC pre-formatted format: {}", record.value().toString());
+                log.trace("event does not follow correct HEC pre-formatted format for record having offset: {} and topic: {}", record.kafkaOffset(), record.topic());
                 event = createHECEventNonFormatted(record);
             }
         } else {
